fa98551b9f33cdea547170ce0d308e5ad38845cd
Tiago Navarro <tiagornandrade@hotmail.com>
2021-05-13 16:18:49 -0300

    resource-group

---
+++ resource-group/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.31.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "animals-group"
+  location = "brazilsouth"
+  tags = {
+    enviroment = "dev"
+    source     = "Terraform"
+    owner      = "Tiago Navarro"
+  }
+}
+
+resource "azurerm_eventhub_namespace" "example" {
+  name                = "acceptanceTestEventHubNamespace"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "Standard"
+  capacity            = 1
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_eventhub" "example" {
+  name                = "acceptanceTestEventHub"
+  namespace_name      = azurerm_eventhub_namespace.example.name
+  resource_group_name = azurerm_resource_group.example.name
+  partition_count     = 2
+  message_retention   = 1
+}

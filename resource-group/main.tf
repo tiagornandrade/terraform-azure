9907589b90baa403a398f59c0ca11fe83233032e
Tiago Navarro <tiagornandrade@hotmail.com>
2021-05-13 16:30:35 -0300

    resource-group

--- resource-group/main.tf
+++ resource-group/main.tf
@@ -20,23 +20,3 @@ resource "azurerm_resource_group" "example" {
     owner      = "Tiago Navarro"
   }
 }
-
-resource "azurerm_eventhub_namespace" "example" {
-  name                = "acceptanceTestEventHubNamespace"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  sku                 = "Standard"
-  capacity            = 1
-
-  tags = {
-    environment = "Production"
-  }
-}
-
-resource "azurerm_eventhub" "example" {
-  name                = "acceptanceTestEventHub"
-  namespace_name      = azurerm_eventhub_namespace.example.name
-  resource_group_name = azurerm_resource_group.example.name
-  partition_count     = 2
-  message_retention   = 1
-}
